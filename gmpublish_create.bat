53368161f66527eb7d44c04e5133dfe603b04f22
Xibitol <47575082+Xibitol@users.noreply.github.com>
2021-04-29 09:16:03 +0200

    GM script deletion

--- gmpublish_create.bat
+++
@@ -1,2 +0,0 @@
-"D:\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe" create -addon "zombielike_map.gma" -icon "zombielike_map.jpg"
-pause
--- gmpublish_update.bat
+++
@@ -1,2 +0,0 @@
-"D:\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe" update -icon "zombielike_map.jpg" -id "2119236943"
-pause
